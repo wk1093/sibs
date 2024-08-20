0d0d9fe70371f9af2ad81fc2e68353b198f7b95a
wk1093 <wyattk1093@gmail.com>
2024-08-20 10:54:36 -0600

    0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='GNU General Public License v3.0',
     packages=['sibs'],
     install_requires=[
-        'distutils',
+        
     ],
     entry_points={
         'console_scripts': [
--- sibs/_version.py
+++ sibs/_version.py
@@ -1,4 +1,4 @@
 
-sibsversion = "0.1.0"
+sibsversion = "0.1.2"
 __version__ = sibsversion
 __author__ = "wk1093"
